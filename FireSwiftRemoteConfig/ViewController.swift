a26f4e6d1ea2868ce630f2f8b56f8e007133d9b1
Ashish Kakkad <ashishkakkad8@gmail.com>
2016-11-26 00:01:49 +0530

    Firebase Remote Config - Swift 3

--- FireSwiftRemoteConfig/ViewController.swift
+++ FireSwiftRemoteConfig/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import FirebaseRemoteConfig
 
 class ViewController: UIViewController {
 
+    var remoteConfig: FIRRemoteConfig!
+    let sampleURLConfigKey = "sampleURL"
+    @IBOutlet weak var btnFetch: UIButton!
+    @IBOutlet weak var lblResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        remoteConfig = FIRRemoteConfig.remoteConfig()
+        let remoteConfigSettings = FIRRemoteConfigSettings(developerModeEnabled: true)
+        remoteConfig.configSettings = remoteConfigSettings!
+        remoteConfig.setDefaultsFromPlistFileName("FireSwiftRemoteConfigDefaults")
+        fetchConfig()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fetchConfig() {
+        lblResult.text = remoteConfig[sampleURLConfigKey].stringValue
+        
+        var expirationDuration = 3600
+        if remoteConfig.configSettings.isDeveloperModeEnabled {
+            expirationDuration = 0
+        }
 
+        remoteConfig.fetch(withExpirationDuration: TimeInterval(expirationDuration)) { (status, error) -> Void in
+            if status == .success {
+                print("Config fetched!")
+                self.remoteConfig.activateFetched()
+            } else {
+                print("Config not fetched")
+                print("Error \(error!.localizedDescription)")
+            }
+            self.display()
+        }
+    }
+    
+    func display() {
+        lblResult.text = remoteConfig[sampleURLConfigKey].stringValue
+    }
+
+    @IBAction func fetchButtonPressed(_ sender: AnyObject) {
+        fetchConfig()
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-FireSwiftRemoteConfig/Pods-FireSwiftRemoteConfig-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FireSwiftRemoteConfigVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FireSwiftRemoteConfigVersionString[];
+
